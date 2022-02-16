7b285190a9121db982a31b50a56a378835b27d17
Tahir Chaudhry <tahirchaudhry@Tahirs-Air.phub.net.cable.rogers.com>
2022-02-16 16:41:10 -0500

    added rlib and improved password-utils

--- TS/lib/password-utils.ts
+++ TS/lib/password-utils.ts
@@ -1,11 +1,48 @@
+// module for secure cryptographics hashes and streamlined password class
+
 var SHA256 = require("crypto-js/sha256");
 
-interface Hash {
-  value: string;
-  keyphrase: string;
-  plaintext?: string;
+
+export let sha256_hash = (str: string) => {
+	return SHA256(str).toString();
 }
 
-class Password {
+export class Hash {
+	protected plain?: string;
+	value: string;
 
+	constructor(value: string, store_plain: boolean = false) {
+		if (store_plain == true) {
+			this.plain = value
+		}
+
+		this.value = SHA256(value).toString();
+	}
+}
+
+export class Password {
+	private hashObj: Hash;
+	hash: string;
+
+
+	compare(toCompare: string) {
+		let hashed = new Hash(toCompare).value;
+		return hashed == this.hash;
+	}
+
+	change(oldPass: string, newPass: string) {
+		let oldHash = new Hash(oldPass).value;
+		if (oldHash == this.hash) {
+			let newHash: Hash = new Hash(newPass);
+			this.hashObj = newHash;
+			this.hash = newHash.value;
+		}
+	}
+
+
+	constructor(value: string, store_plain: boolean = false) {
+		let createdHash: Hash = new Hash(value, store_plain);
+		this.hashObj = createdHash;
+		this.hash = createdHash.value;
+	}
 }
--- TS/lib/roblox-userdatas.ts
+++ TS/lib/roblox-userdatas.ts
@@ -1,8 +1,7 @@
 // Port of the userdatas portion of the API
 
-let lerp = (v1: number, v2: number, t: number) => {
-	return v1 + (v2 - v1) * t
-}
+import { lerp, round } from "./rlib";
+
 
 
 class UDim {
@@ -31,6 +30,7 @@ class UDim {
 	}
 }
 
+
 class UDim2 {
 	X: UDim;
 	Y: UDim;
@@ -56,6 +56,33 @@ class UDim2 {
 		return nud
 	}
 
+	Lerp(goal: UDim2, alpha: number) {
+		let xud1: UDim = this.X;
+		let xud2: UDim = goal.X;
+		let yud1: UDim = this.Y;
+		let yud2: UDim = goal.Y;
+		let xud11: number = this.X.Scale;
+		let xud12: number = this.X.Offset;
+		let xud21: number = goal.X.Scale;
+		let xud22: number = goal.X.Offset;
+		let yud11: number = this.Y.Scale;
+		let yud12: number = this.Y.Offset;
+		let yud21: number = goal.Y.Scale;
+		let yud22: number = goal.Y.Offset;
+
+		let ixscale: number = lerp(xud11, xud21, alpha);
+		let ixoffset: number = lerp(xud12, xud22, alpha);
+		let iyscale: number = lerp(yud11, yud21, alpha);
+		let iyoffset: number = lerp(yud12, yud22, alpha);
+
+		let iUDim1: UDim = new UDim(ixscale, ixoffset);
+		let iUDim2: UDim = new UDim(iyscale, iyoffset);
+
+		let nud = new UDim2(iUDim1, iUDim2);
+		return nud;
+	}
+
+
 	constructor(x: UDim, y: UDim) {
 		this.X = x;
 		this.Y = y;
@@ -63,3 +90,20 @@ class UDim2 {
 		this.Height = y;
 	}
 }
+
+
+class Vector2 {
+	X: number;
+	Y: number;
+	Magnitude: number;
+	// Unit: number;
+
+	constructor(x: number, y: number) {
+		this.X = x;
+		this.Y = y;
+		this.Magnitude = round(Math.sqrt(this.X^2 + this.Y^2), 1);
+	}
+}
+
+let v2 = new Vector2(3, 4);
+console.log(round(v2.Magnitude, 1));
